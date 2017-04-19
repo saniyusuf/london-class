236972017f0b352866d2807bfa20fe34a77614da
Sani Yusuf <saniyusuf@gmail.com>
2017-04-19 13:55:57 +0100

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from "@angular/common";
+
+/**
+ * Components
+ */
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoViewerComponent } from './video-viewer/video-viewer.component';
+import { VideoFiltersComponent } from './video-filters/video-filters.component';
+import { StatGraphsComponent } from './stat-graphs/stat-graphs.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
-    path: '',
-    children: []
+    path: '', redirectTo: 'dash', pathMatch: "full"
+  },
+  {
+    path: 'dash', component: DashboardComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [
+    VideoListComponent,
+    VideoViewerComponent,
+    VideoFiltersComponent,
+    StatGraphsComponent,
+    DashboardComponent
+  ],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,10 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { VideoListComponent } from './video-list/video-list.component';
-import { VideoViewerComponent } from './video-viewer/video-viewer.component';
-import { VideoFiltersComponent } from './video-filters/video-filters.component';
-import { StatGraphsComponent } from './stat-graphs/stat-graphs.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    VideoListComponent,
-    VideoViewerComponent,
-    VideoFiltersComponent,
-    StatGraphsComponent,
-    DashboardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

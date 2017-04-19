d2d51de5fc74d108efbc73154818074083d7dcc0
Sani Yusuf <saniyusuf@gmail.com>
2017-04-19 11:00:50 +0100

    202 workshop completed added ngFor for list of videos

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -7,9 +7,174 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoListComponent implements OnInit {
 
+  public videoList = VIDEOS;
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+const VIDEOS = [
+  {
+    "title": "Angular Observable Data Flow",
+    "author": "Kyle Cordes",
+    "id": "JPuqluYYa-o",
+    "viewDetails": [
+      {
+        "age": 17,
+        "region": "North America",
+        "date": "2016-03-23"
+      }, {
+        "age": 27,
+        "region": "North America",
+        "date": "2016-03-23"
+      }, {
+        "age": 37,
+        "region": "North America",
+        "date": "2016-03-23"
+      }, {
+        "age": 47,
+        "region": "Europe",
+        "date": "2016-03-24"
+      }, {
+        "age": 37,
+        "region": "North America",
+        "date": "2016-03-24"
+      }, {
+        "age": 17,
+        "region": "North America",
+        "date": "2016-03-25"
+      }
+    ]
+  },
+  {
+    "title": "Angular Live Coding",
+    "author": "Kyle Cordes",
+    "id": "CkK1BT6mMAw",
+    "viewDetails": [
+      {
+        "age": 36,
+        "region": "North America",
+        "date": "2016-06-23"
+      }, {
+        "age": 30,
+        "region": "North America",
+        "date": "2016-06-23"
+      }, {
+        "age": 54,
+        "region": "North America",
+        "date": "2016-07-23"
+      }, {
+        "age": 43,
+        "region": "Europe",
+        "date": "2016-0-24"
+      }, {
+        "age": 32,
+        "region": "North America",
+        "date": "2016-08-24"
+      }, {
+        "age": 32,
+        "region": "North America",
+        "date": "2016-08-25"
+      }
+    ]
+  },
+  {
+    "title": "Managing State in Angular",
+    "author": "Kyle Cordes",
+    "id": "eBLTz8QRg4Q",
+    "viewDetails": [
+      {
+        "age": 17,
+        "region": "North America",
+        "date": "2016-03-23"
+      }, {
+        "age": 27,
+        "region": "North America",
+        "date": "2016-03-23"
+      }, {
+        "age": 37,
+        "region": "North America",
+        "date": "2016-03-23"
+      }, {
+        "age": 47,
+        "region": "Europe",
+        "date": "2016-03-24"
+      }, {
+        "age": 37,
+        "region": "North America",
+        "date": "2016-03-24"
+      }, {
+        "age": 17,
+        "region": "North America",
+        "date": "2016-03-25"
+      }
+    ]
+  },
+  {
+    "title": "Modl Buildr",
+    "author": "Paul Spears",
+    "id": "4ENpTiHKyi4",
+    "viewDetails": [
+      {
+        "age": 36,
+        "region": "North America",
+        "date": "2016-06-23"
+      }, {
+        "age": 30,
+        "region": "North America",
+        "date": "2016-06-23"
+      }, {
+        "age": 54,
+        "region": "North America",
+        "date": "2016-07-23"
+      }, {
+        "age": 43,
+        "region": "Europe",
+        "date": "2016-0-24"
+      }, {
+        "age": 32,
+        "region": "North America",
+        "date": "2016-08-24"
+      }, {
+        "age": 32,
+        "region": "North America",
+        "date": "2016-08-25"
+      }
+    ]
+  },
+  {
+    "title": "ngConf 2015",
+    "author": "Oasis Digital Solutions Inc.",
+    "id": "NQOCQq6PLQg",
+    "viewDetails": [
+      {
+        "age": 17,
+        "region": "North America",
+        "date": "2016-03-23"
+      }, {
+        "age": 27,
+        "region": "North America",
+        "date": "2016-03-23"
+      }, {
+        "age": 37,
+        "region": "North America",
+        "date": "2016-03-23"
+      }, {
+        "age": 47,
+        "region": "Europe",
+        "date": "2016-03-24"
+      }, {
+        "age": 37,
+        "region": "North America",
+        "date": "2016-03-24"
+      }, {
+        "age": 17,
+        "region": "North America",
+        "date": "2016-03-25"
+      }
+    ]
+  }
+];
